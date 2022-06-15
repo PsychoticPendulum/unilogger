d8a0366768b80249dba77a1f08ccf39fd082924b
psychicpenguin <lukas.mairock@protonmail.com>
2022-06-15 09:36:01 +0200

    Added File handling

--- unilog.py
+++ unilog.py
@@ -139,6 +139,198 @@ class HELP:
             TERM.Printc(f"{FG.CYAN} {indexstr} {UNICODE.VERTICAL[1]}{UTIL.RESET}  {COMMAND}\n")
 
 
+#   ------------------------------
+#   |          _FILES            |
+#   ------------------------------
+class FILE:
+
+
+    def Read(file, csv=False):
+        try:
+            f = open(file, "r")
+
+            index = 0
+            # Vertical padding
+            TERM.Printc("\n")
+            # Draw information about what is being done
+            TERM.Printc(f"Listing contents of: {UTIL.UNDERLINE}{file}\n\n")
+
+            # Go through file line by line
+            for line in f.readlines():
+                # Indexstring is to display line numbers: 12 | command ... tooltip
+                index       += 1
+                indexstr    = " " * (3 - len(str(index))) + str(index)
+                # Remove newline character from every line
+                line        = line.rstrip("\n")
+
+                # Handle csv files properly 
+                if csv:
+                    parts = line.split(",")
+
+                    # Print line index
+                    TERM.Printc(f"{FG.RED} {indexstr} {UNICODE.VERTICAL[BOX.STYLE.EDGY]}{UTIL.RESET}  ")
+
+                    # Print line
+                    for part in parts:
+                        if index == 1: print(f"{UTIL.REVERSE}",end="")
+                        # Buffer is 16 for each entry of csv
+                        if len(part): buffer = " " * (16 - len(part))
+                        else: buffer = ""
+                        TERM.Printf(f"{part}{buffer}")
+
+                else:
+                    # If file is not an csv, just print the line
+                    TERM.Printc(f"{FG.RED} {indexstr} {UNICODE.VERTICAL[BOX.STYLE.EDGY]}{UTIL.RESET}  {line}")
+
+                TERM.Printf("\n")
+
+            f.close()
+
+        except PermissionError:
+            Log(LVL.ERROR, f"Insufficient permissions to read: {UTIL.BOLD}{file}")
+        
+        except FileNotFoundError:
+            Log(LVL.ERROR, f"File does not exist: {UTIL.BOLD}{file}")
+    
+
+    def Write(msg, file, csv=False):
+        
+        if not FILE.Exists(file):
+            
+            # Warn if file does not exist. Prompt to create file. 
+            Log(LVL.WARNING, "File does not exist ...") 
+            if not TERM.Confirm("Do you want to create it?"):
+                return
+
+            # Ask for headline of file 
+            answer = BOX.Prompt(["Enter header line: "],color=f"{FG.WHITE}")
+            answer = answer[0]
+
+            # Actually write contents to file
+            try:
+                f = open(file, "w")
+                f.write(f"Date,Time,{answer}")
+                f.close()
+            
+            # TODO: Add exception handling
+            except:
+                Log(LVL.ERROR, "Something happenend! The devs have not yet added proper handling of this error.")
+                return
+
+        try:
+            f = open(file, "a")
+
+            # Always add Date and Time to entry
+            f.write(f"\n{SYSTEM.Date()},{SYSTEM.Time()},")
+            
+            # Writing is generally done in csv style (as of now)
+            for txt in msg:
+                f.write(f"{txt},")
+
+            f.close()
+        
+        except PermissionError:
+            Log(LVL.ERROR, f"Insufficient permissions to read: {UTIL.BOLD}{file}")
+        
+        except FileNotFoundError:
+            Log(LVL.ERROR, f"File does not exist: {UTIL.BOLD}{file}")
+
+        except IsADirectoryError:
+            Log(LVL.ERROR, f"File is a directory: {UTIL.BOLD}{file}")
+
+
+    def List(directory):
+        try:
+            # Create array containing all files
+            files = os.listdir(directory)
+            index = 0
+
+            # Draw header line explaining what is being done
+            TERM.Printc("\n")
+            TERM.Printc(f"Listing the contents of: {UTIL.UNDERLINE}{directory}\n\n")
+
+            # Create two arrays to seperate directories from regular files
+            directories = []
+            logfiles = []
+
+            # Fill the arrays according to the filetypes
+            for file in files:
+                # The currently selected file will be drawn in reversed colors
+                style = "" 
+                if file == SYSTEM.NAME: style += f"{UTIL.REVERSE}"
+                if os.path.isdir(f"{SYSTEM.PATH}{file}"):   directories.append(f"  {UTIL.BOLD}{FG.YELLOW}{style}{file}")
+                else:                                       logfiles.append(f"  {style}{file}")
+
+            # Now merge the arrays for easier iteration
+            files = directories
+            for logfile in logfiles:
+                files.append(f"{logfile}")
+
+            for file in files:
+                # Indexstring still displaying line numbers nicely
+                index       += 1
+                indexstr    = " " * (3 - len(str(index))) + str(index)
+
+                # Draw the final output
+                TERM.Printc(f"{FG.MAGENTA} {indexstr} {UNICODE.VERTICAL[BOX.STYLE.EDGY]}{UTIL.RESET}  {file}\n")
+        
+        except PermissionError:
+            Log(LVL.ERROR, f"Insufficient permissions to read: {UTIL.BOLD}{file}")
+        
+        except FileNotFoundError:
+            Log(LVL.ERROR, f"File does not exist: {UTIL.BOLD}{file}")
+
+
+    def Delete(file,nowarn=False):
+
+        # Warn the user that files are going to be destroyed
+        if not nowarn:
+            Log(LVL.WARNING, f"You are about to delete {UTIL.UNDERLINE}{file}")
+            if not TERM.Confirm("Do you want to continue?"):
+                return
+
+        try:
+            os.remove(file)
+        
+        except PermissionError:
+            Log(LVL.ERROR, f"Insufficient permissions to read: {UTIL.BOLD}{file}")
+        
+        except FileNotFoundError:
+            Log(LVL.ERROR, f"Directory does not exist: {UTIL.BOLD}{file}")
+
+        except IsADirectoryError:
+            # Warn the user that this might be a bad idea, like TikTok, but worse
+            Log(LVL.WARNING, "File is a directory!")
+            # Give user another chance to abort
+            if not TERM.Confirm(f"Are you {UTIL.BOLD}really{UTIL.RESET} sure you want to do this?"):
+                return
+            # Get list of files in that directory
+            files = os.listdir(f"{file}")
+            # Iterate through every file
+            for subject in files:
+                Log(LVL.INFO, f"Deleting {UTIL.UNDERLINE}{subject}")
+                # Use recursion to delete subfolders etc.
+                FILE.Delete(f"{file}/{subject}",nowarn=True)
+            os.rmdir(file)
+
+
+    def Exists(path):
+        return os.path.exists(path)
+
+
+    def MakeDirectory(path):
+        if FILE.Exists(path):
+            Log(LVL.WARNING, f"Directory already exists: {UTIL.BOLD}{path}")
+            return
+
+        try:
+            os.mkdir(path)
+        
+        except PermissionError:
+            Log(LVL.ERROR, f"Insufficient permissions to write in: {UTIL.BOLD}{path}")
+
+
+
 def Prompt():
     TERM.Overlay()
 
